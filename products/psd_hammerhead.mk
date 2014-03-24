26cf8af9ba29b1e5e008b89e6697bcbb8e5ac5cf
TheMrcool212 <munirmohamadrani@gmail.com>
2014-03-24 14:23:27 +0800

    psd_hammerhead: Point the script in the right path

--- products/psd_hammerhead.mk
+++ products/psd_hammerhead.mk
@@ -3,5 +3,5 @@ PSD_TARGET_PRODUCT := psd_hammerhead
 
 # Copy kernel modules list over
 PRODUCT_COPY_FILES += \
-    vendor/pa/prebuilt/device/hammerhead/etc/init.d/88LoadMod:system/etc/init.d/88LoadMod \
-    vendor/pa/prebuilt/device/hammerhead/etc/init.d/89InsMod:system/etc/init.d/89InsMod
+    vendor/psd/prebuilt/device/hammerhead/etc/init.d/88LoadMod:system/etc/init.d/88LoadMod \
+    vendor/psd/prebuilt/device/hammerhead/etc/init.d/89InsMod:system/etc/init.d/89InsMod

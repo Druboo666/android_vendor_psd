818103794d877f1ca2a2a92fc71589c3e378f870
Michael S Corigliano (MikeCriggs@XDA) <michael.s.corigliano@gmail.com>
2014-04-01 16:04:37 -0400

    vendor.mk: bump to V3

--- vendor.mk
+++ vendor.mk
@@ -4,6 +4,6 @@ export VENDOR_PSD := psd
 # Look at this makes you awesome
 
 # PSD version info
-PSD_VERSION_MINOR := V2
+PSD_VERSION_MINOR := V3
 PSD_VERSION_MAINTENANCE := Beta
 PSD_VERSION_TAG := Remix

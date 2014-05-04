7e01ac71fa58d2f84d038e3acc19f7b3e5b330e9
TheMrcool212 <themrcool212.android@gmail.com>
2014-05-04 16:33:17 +0800

    V4 Finally!

--- vendor.mk
+++ vendor.mk
@@ -1,9 +1,7 @@
 # Export rom name
 export VENDOR_PSD := psd
 
-# Look at this makes you awesome
-
 # PSD version info
-PSD_VERSION_MAJOR := V3
+PSD_VERSION_MAJOR := V4
 PSD_VERSION_MAINTENANCE := Beta
 PSD_VERSION_TAG := Remix

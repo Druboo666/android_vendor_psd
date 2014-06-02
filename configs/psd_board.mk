ed9cef0ca8619c70e4d648613de18b503357e3e1
Paul Beeler <pbeeler80@gmail.com>
2014-06-01 22:43:12 -0600

    Make graphite and pthread work with vendor/psd/products/psd_device.mk by using a pa hooks file:
    See previous commit for hammerhead.  This is much better than having to use device trees.
    Use := and not += in product files.
    
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>

--- configs/psd_board.mk
+++ configs/psd_board.mk
@@ -1,7 +1,10 @@
 # Call product first and foremost
 # Then check for deivce settings
+
 $(call inherit-product-if-exists, vendor/psd/products/$(TARGET_PRODUCT)_hooks.mk)
+
 ifndef GCC_VERSION_AND
+# GCC_VERSION_AND is not defined, set to 4.8 default
 GCC_VERSION_AND := 4.8
 SM_AND_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-$(GCC_VERSION_AND)
 SM_AND := $(shell $(SM_AND_PATH)/bin/arm-linux-androideabi-gcc --version)
@@ -52,17 +55,30 @@ endif
 
 # begin pthread support
 # This patch also allows more modules to be added to THREADS_MODULE_LIST if needed in future updates.
-# And also in other places like BoardConfig.mk by using "THREADS_MODULE_LIST += insert_module_name"
-THREADS_MODULE_LIST := oatdump dex2oat
+# And also in the device product makefile vendor/psd/products/psd_device.mk by adding "THREADS_MODULE_LIST := insert_module_name"
+ifndef THREADS_MODULE_LIST
+	THREADS_MODULE_LIST := oatdump dex2oat
+else
+	THREADS_MODULE_LIST += oatdump dex2oat
+endif
 # end pthread support
 
 # begin graphite support
 # This patch also allows more modules to be added to DISABLE_GRAPHITE_MODULES if needed in future updates.
-# And also in other places like BoardConfig.mk by using "DISABLE_GRAPHITE_MODULES += insert_module_name"
-DISABLE_GRAPHITE_MODULES := libstagefright_amrwbenc \
-	libFFTEm \
-	libwebviewchromium \
-	libstagefright_mp3dec \
-	libjni_filtershow_filters \
-	libwebrtc_spl
+# And also in the device product makefile vendor/psd/products/psd_device.mk by adding "DISABLE_GRAPHITE_MODULES := insert_module_name"
+ifndef DISABLE_GRAPHITE_MODULES
+	DISABLE_GRAPHITE_MODULES := libstagefright_amrwbenc \
+		libFFTEm \
+		libwebviewchromium \
+		libstagefright_mp3dec \
+		libjni_filtershow_filters \
+		libwebrtc_spl
+else
+	DISABLE_GRAPHITE_MODULES += libstagefright_amrwbenc \
+		libFFTEm \
+		libwebviewchromium \
+		libstagefright_mp3dec \
+		libjni_filtershow_filters \
+		libwebrtc_spl
+endif
 # end graphite support
